fa27ead1ad9b54b2ca98d1ab054ad0330e6a8389
Lucas <lac.lucas96@gmail.com>
2022-07-03 11:56:34 -0300

    feat: add accessibility to destination page

--- js/destination.js
+++ js/destination.js
@@ -5,13 +5,21 @@ const destinations = data.destinations;
 selectors.forEach((selector, index) => {
   selector.addEventListener("click", () => {
     clearActiveClasses();
+    clearAriaSelectedAttr();
     selector.classList.add("active");
     handleUpdateDestination(index);
+    handleDestinationAttributes(index);
   });
 });
 
 function clearActiveClasses() {
   selectors.forEach((selector) => selector.classList.remove("active"));
+}
+
+function clearAriaSelectedAttr() {
+  selectors.forEach((selector) => {
+    selector.firstElementChild.setAttribute("aria-selected", false);
+  });
 }
 
 function handleUpdateDestination(index) {
@@ -26,4 +34,11 @@ function handleUpdateDestination(index) {
   description.innerText = `${destinations[index].description}`;
   distance.innerText = `${destinations[index].distance}`;
   travel.innerText = `${destinations[index].travel}`;
+}
+
+function handleDestinationAttributes(index) {
+  const article = document.getElementById("tabpanel");
+  selectors[index].firstElementChild.setAttribute("aria-selected", true);
+  article.setAttribute("tabindex", index);
+  article.setAttribute("aria-labelledby", `tab-${index}`);
 }
